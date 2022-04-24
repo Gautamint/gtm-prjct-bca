dde5eb0d3183b59a8469acd51faa7bcda29d3a76
Gautamint <gautaminit@gmail.com>
2022-04-24 10:34:48 +0530

    commiting for push

---
+++ contact.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>www.italianrestorent.com</title>
+	<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+   <style type="text/css">
+   	#main{ width: 100%; height: auto; background-color: white; }
+   	.nav-container{ width: 97%; height: 100px; background-color: white; padding: 10px; margin: auto;  position: relative; position: fixed; border-bottom: 1px solid lightgrey;z-index: 999;}
+   	.nav-left{width: 22%; height: 100px; background-color:;  float: left;} 
+   	.nav-middile{width: 56%;height: 100px; background-color: ; float: left; position: relative;left: 50px;}
+        .line{margin-left:17px;color: lightgrey;font-size: 24px;}
+    .nav-right{width: 22%; height: 100px; background-color: transparent;  float: left; position: relative;
+    }     
+   	.main-logo{color: black; position: absolute; top: 48%; left: 10%; font-size: 32px; transform: translate(-80%,-50%); font-weight: bold; font-family: "Times New Roman", Times, serif;}
+   	.logo-img{ height: 28px; position: absolute; top: 15%; margin-left: 10px;}
+   	  .navbar{ list-style: none; display: inline; margin-left:-53px;}
+   	  .navi{display: inline; line-height: 100px; margin-left: 20px; text-decoration: none;}
+   	  .navi a{text-decoration: none; color: black; font-weight: 6px; font-style:  1px solid bold; font-weight: 900;}
+   	  .navi a:hover{color: orange; transition: 0.5s;}
+   	  	 b.rotate{transform: rotate(90deg);}
+      .ikon{ position: absolute; top: 35%;left: 20%; color: orange; font-size:25px; transform: translate(-50,-10); transform: rotate(135deg);}
+      .ikon:hover{ color: black;transition: 1s;}   
+      .num-call{ position: absolute;top: 33%; left: 50%; transform: translate(-50%,-50%); font-size: 16px; font-weight: 900; } 
+      .num-call:hover{color: orange; transition: 1s;} 
+        .menu-item-real{width: 350px; height: 200px; background-color: white; position: absolute;  margin-left: 120px;`border: 1px solid lightgrey; display: none; }
+        .mnr-up{width: 100%; height: 50%;}
+        .mnr-down{width: 100%; height: 50%;} 
+        .item-up{width: 32.7612%; height: 100%; background-color: white; border: 1px solid lightgrey; float: left;} 
+        .menu-item:hover .menu-item-real{ display: block!important;z-index: 99}  
+        .menu-img{padding-top: 7%; padding-left: 15%; box-sizing: border-box;}
+        /*-------------------------------------------- nav end-------------------------------- */
+
+         .footer{width: 100%;height: 400px;background-color:#191818;position: relative;margin-top: 50px;}   
+          .footer-left{width: 33%;height: 65%;float: left;margin-left: 7px;border-right: 2px solid #484848;margin-top:40px;}
+              .footer-logo{height: 30px; position: absolute; top: 10%;left: 12%;}
+              .footer-left p{font-size: 16px;color: grey;margin-top: 60px;margin-left: 40px;}
+              .footer-left pre{font-size: 16px;color: grey;margin-top: 30px}
+              .footer-left b{color: white;margin-left: 40px;}
+              .footer-left a{text-decoration: none;color: grey;}
+              .footer-left a:hover{color:orange;}
+              .footer-italian{font-size: 35px;top: 8.5%;position: absolute;}
+          .footer-midle{width: 33%;height: 65%;float: left;border-right: 2px solid black;margin-top: 40px;}
+              .footer-midle b{margin-top: -5px;margin-left: 0px;font-size:35px;color: white;}
+              .footer-midle p{margin-top: -5px;margin-left: 40px;font-size:16px;color: grey;word-spacing: 10px;}
+              .footer-midle a{text-decoration: none;color: grey;}
+              .footer-midle a:hover{color: orange;}
+              .footer-midle pre{color: white;font-size: 24px;margin-left: 40px;}
+          .footer-right{width: 33%;height: 65%;float: left;margin-top: 40px;position: relative;} 
+              .footer-right b{font-size:35px;color: white;position: absolute;left: 40px;top: -5px;} 
+              .footer-right-email{padding: 18px 10px;border-radius: 40px;border: none;color: grey;position: absolute;                 left: 40px;top: 60px} 
+              .footer-right button{background-color: red; color: white;border-radius: 30px;border: none;padding: 15px 25px;
+                                   position: absolute;left: 250px;padding: 18px 16px;top: 60px;}
+              .footer-right button:hover{background-color: orange;cursor: pointer;}
+              .footer-right pre{color: white;font-size: 24px;margin-left: 40px;position: absolute;top: 110px}
+             
+          .footer-last{width: 100%;height: 20%;background-color:#2f2e2d;float: left;margin-top: 20px;position: relative;} 
+              .footer-last p{font-size: 16px;color: grey;top: 15px;left: 40px;position: absolute;}
+              .footer-last a{color: grey} 
+              .footer-last a:hover{color: orange;}
+  </style>      
+</head>
+<body>
+<div id="main">
+       <div class="nav-container"> 
+          <div class="nav-left">
+            <div class="main-logo">Italian<img src="images/logo-280x60.png" class="logo-img"></div>
+          </div>
+
+          <div class="nav-middile">
+              <ul class="navbar">
+              <li class="navi"><a href=""> HOME <span class="line">|</span></a></li>
+              <li class="navi"><a href=""> ABOUT US <span class="line">|</span></a></li>
+              <li class="navi menu-item"><a href="#"> MENU<i class="fa fa-caret-down"></i><!-- <i class="fa fa-caret-up"></i> --> <span class="line">|</span></a>
+                  <div class="menu-item-real">
+                      <div class="mnr-up">
+                      <a href=""><div class="item-up "><img src="images/menu-food-01.png" class="menu-img"></div></a>
+                      <a href=""><div class="item-up "><img src="images/menu-food-02.png" class="menu-img"></div></a>
+                      <a href=""><div class="item-up "><img src="images/menu-food-03.png" class="menu-img"></div></a>
+                      </div>
+                      <div class="mnr-down">
+                        <a href=""><div class="item-up "><img src="images/menu-food-04.png" class="menu-img"></div></a>
+                        <a href=""><div class="item-up "><img src="images/menu-food-05.png" class="menu-img"></div></a>
+                        <a href=""><div class="item-up "><img src="images/menu-food-06.png" class="menu-img"></div></a>
+                      </div>
+
+                  </div>
+                         
+                </li>
+              <li class="navi"><a href=""> NEWS <span class="line">|</span></a></li>
+              <li class="navi"><a href=""> PAGES <span class="line">|</span></a></li>
+              <li class="navi"><a href=""> CONTACT US </a></li>
+                </ul>
+          </div>
+
+          <div class="nav-right">
+               <i class="fas fa-phone-alt ikon"></i>
+               <p class="num-call">(0731) 546784</p>
+          </div>
+        </div>
+       <!-- -------------------------------------------- nav end-------------------------------- -->
+
+       <div class="footer">
+      <div class="footer-left">
+          <div><b class="footer-italian">Italian</b><img src="images/logo-inverse-280x60.png" class="footer-logo">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint esse, ullam officiis a? Officiis fugiat consectetur deleniti, illum iste electus, nemo est?</p>
+          <pre><b>Address:</b>63-new hina palace colony
+          <b style="color: white;margin-left: -50px">Email:</b><a href="#">info@demolink.org</a>
+          <b style="color: white;margin-left: -50px">Phone:</b><a href="#">1-800-1234-567</a></pre>
+          </div>
+      </div>
+
+      <div class="footer-midle">
+        <p><b>OUR MENU</b></p>
+        <p style="margin-top: 30px"><a href="">BURGERS</a>  / <a href="">PIZZAS</a>  / <a href="">TOASTS</a>  / <a href="">SALADS</a>  / <a href="">DRINKS</a>  / <a href="">DESSERTS</a> </p>
+        <pre>OPENING HOURS</pre>
+        <p><i class="far fa-clock" style="font-size:20px;color:orange"></i>  9:00am-7:00pm</p>
+      </div>
+
+      <div class="footer-right">
+         <p><b>NEWSLETTER</b></p>
+         <input type="Email" name="" value="Your Email" class="footer-right-email">
+         <button type="button">SUBSCRIBE</button>
+         <pre>FOLLOW US</pre>
+         <i class="fab fa-facebook-f" style="font-size:23px;color:grey;margin-left: 9%;margin-top: 160px;"></i>
+         <i class="fab fa-twitter" style="font-size:23px;color:grey;margin-left: 5px;"></i>
+         <i class="fab fa-google-plus-g" style="font-size:23px;color:grey;margin-left: 5px;"></i>
+         <i class="fab fa-instagram" style="font-size:23px;color:grey;margin-left: 5px"></i>
+      </div>
+
+      <div class="footer-last">
+         <p>Restoral ©2020<a href="#"> Privacy Policy</a> . Design by <a href="#"> Restoral</a></p>
+         <i class="fab fa-instagram" style="font-size:23px;color:lightgrey;margin-left: 85%;margin-top: 30px"></i>
+         <i class="fab fa-facebook-f" style="font-size:23px;color:lightgrey;margin-left: 10px"></i>
+         <i class="fab fa-twitter" style="font-size:23px;color:lightgrey;margin-left: 10px"></i>
+         <i class="fab fa-google-plus-g" style="font-size:23px;color:lightgrey;margin-left: 10px"></i>
+      </div>
+    </div>
+
+        <div style="clear: both;"></div>
+</div>
+</body>
+</html>
